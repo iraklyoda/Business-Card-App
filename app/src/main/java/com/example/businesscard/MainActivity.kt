c03b59d69b42dc54310e665d0194f22dc78dea93
Irakli <Iraklyoda42@gmail.com>
2024-09-26 01:27:20 +0400

    feat: create introduction composable

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -1,16 +1,29 @@
 package com.example.businesscard
 
+import android.graphics.Picture
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.businesscard.ui.theme.BusinessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,6 +44,33 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun Introduction(
+    profilePicture: Painter,
+    fullName: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.padding(start = 8.dp, end = 8.dp).fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(
+            painter = profilePicture,
+            contentDescription = "Profile Picture",
+            modifier.size(200.dp)
+        )
+        Text(
+            text = fullName,
+            fontSize = 24.sp,
+        )
+        Text(
+            text = title,
+            fontSize = 12.sp
+        )
+    }
+}
+
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
@@ -42,6 +82,12 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     BusinessCardTheme {
-        Greeting("Android")
+        Column(Modifier.fillMaxSize()) {
+        Introduction(
+            profilePicture = painterResource(R.drawable.yoda_the_empire_strikes_back_28a7558),
+            fullName = "Irakli Iobashvili",
+            title = "Android app developer",
+        )
+        }
     }
 }
